4a835bc6b58b955d549f203a00b69fd3bdff0583
przemyslaw.jopa <przemyslaw.jopa@gmail.com>
2019-01-11 19:29:11 +0100

    Add interface IBuilder.

---
+++ EasyEnglishWPF/Patterns/IBuilder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EasyEnglishWPF.Classes;
+
+namespace EasyEnglishWPF.Patterns
+{
+    interface IBuilder
+    {
+        Test Test { get; }
+
+        void NewTest();
+        void SetDifficulty();
+        void SetType();
+    }
+}
