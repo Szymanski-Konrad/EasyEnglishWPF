734f16c1d03bcef1f704c2fa3a8d6b56c0fe973d
przemyslaw.jopa <przemyslaw.jopa@gmail.com>
2019-01-14 01:29:02 +0100

    Update Test class.

--- EasyEnglishWPF/Classes/Test.cs
+++ EasyEnglishWPF/Classes/Test.cs
@@ -3,12 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using EasyEnglishWPF.Patterns;
 
 namespace EasyEnglishWPF.Classes
 {
-    public abstract class Test
+    public abstract class Test : ILevel, IType
     {
+
         protected string test_name = "test";
+
+        public int level { get; set; }
+        public string type { get; set; }
 
         virtual public string Hint()
         {
