034e7870764d6f802a123d1169a59d47de6c6456
przemyslaw.jopa <przemyslaw.jopa@gmail.com>
2019-01-17 00:33:32 +0100

    Add Lecturer class.

---
+++ EasyEnglishWPF/Classes/Lecturer.cs
@@ -0,0 +1,28 @@
+﻿using EasyEnglishWPF.Patterns;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyEnglishWPF.Classes
+{
+    /// <summary>
+    /// Invokes commands for <see cref="User"/>.
+    /// </summary>
+    /// <seealso cref="EasyEnglishWPF.Patterns.ICommand" />
+    class Lecturer
+    {
+        private ICommand mode;
+
+        public void SetCommand(ICommand command)
+        {
+            mode = command;
+        }
+
+        public void Execute()
+        {
+            mode.Execute();
+        }
+    }
+}
