f10c7e05d970b6aa46737ca0db335b95da35e206
przemyslaw.jopa <przemyslaw.jopa@gmail.com>
2019-01-17 00:30:57 +0100

    Add ICommand interface.

---
+++ EasyEnglishWPF/Patterns/ICommand.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyEnglishWPF.Patterns
+{
+    /// <summary>
+    /// Command interface.
+    /// </summary>
+    public interface ICommand
+    {
+        /// <summary>
+        /// Executes this instance.
+        /// </summary>
+        void Execute();
+
+        /// <summary>
+        /// Stops this instance.
+        /// </summary>
+        void Stop();
+    }
+}
