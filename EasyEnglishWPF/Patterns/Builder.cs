469dc87745c6ddf3340481d47873a0867ddeb55b
przemyslaw.jopa <przemyslaw.jopa@gmail.com>
2019-01-16 21:10:01 +0100

    Add brand new Builder class.

---
+++ EasyEnglishWPF/Patterns/Builder.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EasyEnglishWPF.Classes;
+
+namespace EasyEnglishWPF.Patterns
+{
+    /// <summary>
+    /// Test Builder.
+    /// </summary>
+    abstract class Builder
+    {
+        //UPGRADE: moze cos z nullem zrobic
+
+        protected Test test;
+
+        /// <summary>
+        /// Creates the new <see cref="Test"/>.
+        /// </summary>
+        /// <param name="type">The type of the <see cref="Test"/>. Can take "open" or "close".</param>
+        public void CreateNewTest(string type, string strategy)
+        {
+            if (test == null)
+            {
+                if (type == "open")
+                {
+                    test = new WriteAnswerTest();
+                }
+                if (type == "close")
+                {
+                    if (strategy == "first"
+                        || strategy == "last"
+                        || strategy == "random")
+                    {
+                        test = new SingleChoiceTest(strategy);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets the <see cref="Test"/>.
+        /// </summary>
+        /// <returns><see cref="Test"/></returns>
+        public Test GetTest()
+        {
+            return test;
+        }
+    }
+}
