67fd9fa753a8cbf6eaada107e4e68617edcede2c
przemyslaw.jopa <przemyslaw.jopa@gmail.com>
2019-01-17 04:18:44 +0100

    Add History window.

---
+++ EasyEnglishWPF/Pages/HistoryWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EasyEnglishWPF.Pages
+{
+    /// <summary>
+    /// Interaction logic for HistoryWindow.xaml
+    /// </summary>
+    public partial class HistoryWindow : UserControl, ISwitchable
+    {
+        public HistoryWindow()
+        {
+            InitializeComponent();
+            var list = Database.LoadHistory("user");
+            foreach (string item in list)
+            {
+                historyTextBox.Text += item + "\n";
+            }
+        }
+
+        public void UtilizeState(object state)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Switcher.Switch(new Pages.MainMenu());
+        }
+    }
+}
